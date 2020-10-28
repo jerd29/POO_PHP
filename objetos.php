159b04c2ba912d531a471358e70d602c5b654a40
Jerd <jerd13jerd13@gmail.com>
2020-10-27 23:05:10 -0430

    objetos

---
+++ objetos.php
@@ -0,0 +1,27 @@
+<?php
+
+    class miClase {
+        // mis atributos
+        public $resultado=0;
+
+        // mis metodos
+        public function miMetodo($v1,$v2) {
+
+            $this->resultado=$v1 + $v2;
+            
+            return $this->resultado;
+        }
+
+    }
+
+    // Termina mi clase
+
+    // instanciar o declarar un objeto
+
+    $miObjeto = new miClase();
+
+    echo $miObjeto->miMetodo(5,10);
+
+
+
+?>
