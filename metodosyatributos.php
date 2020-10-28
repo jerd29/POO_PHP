ed249b81b0e0a26a905857c298e5da38898c2222
Jerd <jerd13jerd13@gmail.com>
2020-10-27 22:58:16 -0430

    metodosyatributos

---
+++ metodosyatributos.php
@@ -0,0 +1,18 @@
+<?php
+
+    class miClase {
+        // declaramos atributos
+        public $atributo1="Hola Mundo";
+        
+        // declaramos metodos
+        public function miMetodo() {
+            // como podemos mandar a llamar un atributo
+            // utilizando la pseudo variable this
+
+            return $this->atributo;
+        }
+    }
+
+
+
+?>
