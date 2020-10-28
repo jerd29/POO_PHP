a70489317af77d996a4e8ff1e49e781d06143a34
Jerd <jerd13jerd13@gmail.com>
2020-10-27 21:22:20 -0430

    clases

---
+++ clases.php
@@ -0,0 +1,22 @@
+<?php 
+// una clase es nuestro molde de trabajo
+// para nuestros metodos y atributos
+
+    class miClase {
+        // definir los atributos
+
+
+        // definir los metodos
+    }
+    
+    class miClase2 {
+
+    }
+
+// podemos declarar cualquier numero de clases dentro de un 
+// archivo php
+
+
+
+
+?>
