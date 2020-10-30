1c02d94db67a32389c403c8df4cabd80bec30a7e
Jerd <jerd13jerd13@gmail.com>
2020-10-29 19:45:58 -0430

    retornardatos completado

--- retornodatos.php
+++ retornodatos.php
@@ -15,18 +15,41 @@
 
         }
 
-        public function rerotnaEntero($valor){
+        public function retornaEntero($valor){
             if($valor > 0){
                 return 1;
             }else{
                 return 0;
             }
         }
+
+        public function retornaArreglo($ciclos){
+            $datos = array();
+
+            for ($i=0; $i < $ciclos; $i++) { 
+                $datos[$i]=$i;
+            }
+            return $datos;
+        }
+
+        public function retornaJson(){
+            $arr=array(
+                "hdd" => 500,
+                "pantalla" => 21,
+                "ram" => 8
+            );
+
+            return json_encode($arr);
+        }
     }
 
     $cadena = new retornoDatos();
     var_dump($cadena->retornaString(20));
-    echo "<br>"
+    echo "<br>";
     var_dump($cadena->retornaEntero(1));
+    echo "<br>";
+    var_dump($cadena->retornaArreglo(10));
+    echo "<br>";
+    var_dump($cadena->retornaJson());
 
 ?>
